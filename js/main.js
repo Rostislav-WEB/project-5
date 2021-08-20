ff6e438a31bb5d3b42d61f5d93528cce3f1304ff
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-08-20 21:11:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-  let currentFloor = 2; // ттекущий этаж
+  let currentFloor = 2; // текущий этаж
   let floorPath = $('.home-image path'); // каждый отдельный этаж в svg
   let counterUp = $('.counter-up'); // этаж вверх
   let counterDown = $('.counter-down'); // этаж вниз
+  let modal = $('.modal');
+  let modalCloseButton = $('.modal-close-button');
+  let viewFlatsButton = $('.view-flats');
+  viewFlatsButton.on('click', toggleModal);
   // функция при наведении мышки на этаж
   floorPath.on('mouseover', function() {
     floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
     currentFloor = $(this).attr('data-floor'); // получение значения текущего этажа
     $('.counter').text(currentFloor); // записываем этажа в счетчик 
   });
+
+  floorPath.on('click', toggleModal); // при клике на этаж вызвать окно
+
+  modalCloseButton.on('click', toggleModal); // клик на кнопку закрыть
+
+
   // отслеживание клик по кнопке вверх
   counterUp.on('click', () =>  {
     if(currentFloor < 18) {
@@ -32,5 +42,8 @@ $(document).ready(function () {
       // подсвечиваем текущий этаж
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
     }
-  })
+  });
+  function toggleModal() { // функция открыть закрыть окно
+    modal.toggleClass('is-open')
+  }
 });
